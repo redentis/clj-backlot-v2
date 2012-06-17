ab6ae937cf6a5fd8aeb832839d99009afc12d430
Marcus Edwards <marcus@ooyala.com>
2012-06-17 08:55:15 +0100

    Converted to Leiningen 2 configuration

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
-(defproject ooyala-backlot-v2 "1.0.0-SNAPSHOT"
-  :description "API wrapper around the Ooyala Backlot V2 APIs"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+(defproject ooyala-backlot-v2/ooyala-backlot-v2 "2.0.1-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.apache.httpcomponents/httpclient "4.0.3"]
                  [commons-codec "1.4"]
                  [commons-io "1.4"]
-                 [cheshire            "1.1.4"]
+                 [cheshire "1.1.4"]
                  [biz.source_code/base64coder "2010-12-19"]]
   :aot [org.apache.http.client.methods.HttpPatch]
+  :min-lein-version "2.0.0"
   :jvm-opts ["-Dslime.encoding=UTF-8"]
-)
-;  :jvm-opts ["-Xdebug" "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8888"]
+    :description "API wrapper around the Ooyala Backlot V2 APIs")
